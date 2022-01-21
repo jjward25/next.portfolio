3a40543078a6e740460d7e327f158f49001fb06f
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-01-20 21:19:05 -0700

    n

---
+++ components/widgets/mobilemenu.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Menubar.module.scss';
+import Link from 'next/link'
+import Image from 'next/image';
+
+export default function MobileMenu() {
+return (
+
+        <div className={styles['mobile-menu']}>
+
+          
+
+        </div>
+)};
+
+
+
+
