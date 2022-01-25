7fd96fc4fae1c3cdd835230e959fddf42920470d
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-01-25 12:36:53 -0700

    mobile1draft - transparent icons

--- components/menubar.js
+++ components/menubar.js
@@ -10,26 +10,28 @@ export default function Menubar() {
     }
 
 return (
-            <div className={styles['menubar-menu']}>        
-                <p className={styles['bc-link']}>
-                <a href="https://www.linkedin.com/in/joe-ward1/" title="LinkedIn" alt="LinkedIn" target="_blank" className={styles['bc-work-icon']}>
-                <Image src="/icon-linkedin-blue.png" alt="LinkedIn" layout="fill"></Image></a>
-                LinkedIn</p>
-
-                <p className={styles['bc-link']} >
-                <a href="https://github.com/jjward25" title="Github" alt="Github" target="_blank" className={styles['bc-work-icon']}>
-                <Image src="/icon-github.png" alt="Github" layout="fill"></Image></a>
-                Github</p>
+            <div className={styles['menubar-menu']}>   
                 
-                <p className={styles['bc-link']} >
-                <a href="https://public-docs2.s3.amazonaws.com/Joseph+Ward.pdf" title="Resume" alt="Resume" target="_blank" className={styles['bc-work-icon']}>
-                <Image src="/icon-cv.png" alt="Resume" layout="fill"></Image></a>
-                Resume</p>
+                    <p className={styles['bc-link']}>
+                        <a href="https://www.linkedin.com/in/joe-ward1/" title="LinkedIn" alt="LinkedIn" target="_blank" className={styles['bc-work-icon']}>
+                        <Image src="/icon-linkedin-blue.png" alt="LinkedIn" layout="fill"></Image></a>
+                    LinkedIn</p>
 
-                <p className={styles['bc-link']} id="email" onClick={copy} >
-                <a alt="Email" target="_blank" className={styles['bc-work-icon']}>
-                    <Image src="/icon-email.png" alt="Email Me" layout="fill"></Image></a>
-                E-Mail</p>                
+                    <p className={styles['bc-link']} >
+                        <a href="https://github.com/jjward25" title="Github" alt="Github" target="_blank" className={styles['bc-work-icon']}>
+                        <Image src="/icon-github-clear.png" alt="Github" layout="fill"></Image></a>
+                    Github</p>
+                    
+                    <p className={styles['bc-link']} >
+                        <a href="https://public-docs2.s3.amazonaws.com/Joseph+Ward.pdf" title="Resume" alt="Resume" target="_blank" className={styles['bc-work-icon']}>
+                        <Image src="/icon-cv-clear.png" alt="Resume" layout="fill"></Image></a>
+                    Resume</p>
+
+                    <p className={styles['bc-link']} id="email" onClick={copy} >
+                        <a alt="Email" target="_blank" className={styles['bc-work-icon']}>
+                        <Image src="/icon-email.png" alt="Email Me" layout="fill"></Image></a>
+                    E-Mail</p>   
+                             
             </div>
 )};
 
