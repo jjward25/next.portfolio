69296ef141a5129500c0df85967a2c785ccc8639
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-01-27 12:30:28 -0700

    descriptionsDraft

--- components/blog/process.js
+++ components/blog/process.js
@@ -8,7 +8,13 @@ return (
         <div className={styles['blog-title']}>{`Process Design & Tooling`}</div>
 
         <p className={styles['blog-description']}>
-            
+            {`A major goal for modern businesses is to be as nimble as possible, able to adopt and ideally drive change.  This requires in-depth knowledge of 
+            what you do, and what you don't do.`}
+        </p>
+
+        <p className={styles['blog-description']}>
+            {`The templates and case studies in this section cover issues like monitoring skill supply, designing scalable processes and org charts, and 
+            leveraging modern tools and tech for responsible automation.`}
         </p>
 
     </div>
