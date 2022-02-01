6f18deb6866f72bc38569e35a0a9e278e8f0c53e
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-02-01 08:12:30 -0700

    coreDsc

--- components/blog/core.js
+++ components/blog/core.js
@@ -15,8 +15,16 @@ return (
             </p>
 
             <p className={styles['blog-description']}>
+                {`What was your company founded for? What problems are you solving?  Are you good at that?  Would you be better at something else, 
+                maybe with a little help? Plans may be useless but planning is essential.  Keep an eye to the future, imagine how processes might work, and go with
+                what makes the most all-around sense.`}
+            </p>
+
+            <p className={styles['blog-description']}>
                 {`The next step is an ongoing process that can be done concurrently: honing in on your value to your clients and what your people
-                are good at. From there you'll know where to find partners, and where you have advantages that you can build on.`}
+                are good at.  Use Value Stream Maps to identify what tools, tasks, and other skills required throughout your company, then use those to when planning for
+                 the future, and to help employees drive the changes they want to see in their roles. From there you'll know where to find partners, and where you have 
+                 advantages that you can build on.`}
             </p>
 
         </div>
